7759ab0478e0f0f357c2243ed0a40fd4c17f658a
PokeLegoCuber <PokeLegoCuber@yahoo.com>
2020-06-04 20:49:10 -0400

    Everything but the button wrok for now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props)
+  {
+      super(props);
+      this.state = {input:[]};
+  }
+  
+  render() {
+    const {input} = this.state;
+
+    return (
+      <div className="App">
+        <h1 className = "h1-color">&#x1F697; Welcome to City Search! &#x1F697;</h1>
+        <div className="inputline">
+          <span className = "input-line">Please type in a city: </span><input></input>
+          {/* <button class="btn btn-success" onClick= { ()=>this.submit() }> Enter </button> */}
+        </div>
+        <Search input = "New York"/>
+      </div>
+    );
+  }
 }
 
 export default App;
